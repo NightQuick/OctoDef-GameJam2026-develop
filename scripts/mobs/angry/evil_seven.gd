4013743f329749b49bc3c278d4f631294ca9fdd7
Hopper <Man-popular@mail.ru>
2026-03-17 15:22:31 +0300

    add mouse actions (lkm rpm) in project settings

--- scripts/mobs/angry/evil_seven.gd
+++ scripts/mobs/angry/evil_seven.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
 @onready var NavigationAgent: NavigationAgent2D = $NavigationAgent2D
-
-var self_speed = 50
+#@onready 
+var self_speed = 2
 
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	NavigationAgent.target_position = mouse_position
 	
+	
 	var self_position = global_position
 	var next_path_position = NavigationAgent.get_next_path_position()
-	var new_velocity = self_position.direction_to(next_path_position)
+	var new_velocity = self_position.direction_to(next_path_position) * self_position
+	var LKM = Input.is_action_just_pressed("mouse_left_button") 
+	if LKM:
+		print("2132321")
 	
 	if NavigationAgent.is_navigation_finished():
 		return 
@@ -19,6 +23,7 @@ func _physics_process(delta: float) -> void:
 		NavigationAgent.set_velocity(new_velocity) 
 	else:
 		_on_navigation_agent_2d_velocity_computed(new_velocity)
+	move_and_slide()
 	
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
