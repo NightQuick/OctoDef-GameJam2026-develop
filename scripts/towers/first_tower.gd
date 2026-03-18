e7eb2eaff73bd02eb2acf386c41406e6bca9018e
Prost0Iva <ivaprostoiva@gmail.com>
2026-03-18 20:45:47 +0300

    towers rotate

--- scripts/towers/first_tower.gd
+++ scripts/towers/first_tower.gd
@@ -1,17 +1,46 @@
 extends Node2D
-var _is_body_in_area_ = false
+
+@export var aggr_range: float = 1
+@export var attack_damage: float = 1
+@export var attack_speed: float = 1 
+
+var enemy_in_range: Array = []
 
 func _ready() -> void:
-	print(str(name), ": Инициализирована!")
+	$Body/Range.scale = $Body/Range.scale*aggr_range
 
-func _physics_process(delta: float) -> void:
-	if _is_body_in_area_ == true:
-		print
-		
-func _on_area_body_entered(body: Node2D) -> void:
-	_is_body_in_area_ = true
-	print(str(name), ": тело ", body, " вошло в область")
+func _process(_delta: float) -> void:
+	if len(enemy_in_range) != 0:
+		rotate_head(enemy_in_range)
 
-func _on_area_body_exited(body: Node2D) -> void:
-	_is_body_in_area_ = false
-	print(str(name), ": тело ", body, " покинуло в область")
+func _on_body_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		enemy_in_range.append(body)
+
+func _on_body_body_exited(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		enemy_in_range.erase(body)
+
+func rotate_head(bodies: Array):
+	#Пока что тут настройка поворота на первого врага, что вошёл в зону поражения башни
+	var dir = bodies[0].global_position - global_position
+	var angle_rad = dir.angle()
+	var angle_deg = rad_to_deg(angle_rad)
+	
+	if 112.5 < angle_deg and angle_deg <= 157.5:
+		$Head.region_rect = Rect2(0, 0, 32, 32)
+	if (-157.5 <= angle_deg and angle_deg <= -180) or (157.5 < angle_deg and angle_deg <= 180):
+		$Head.region_rect = Rect2(32, 0, 32, 32)
+	if -157.5 < angle_deg and angle_deg <= -112.5:
+		$Head.region_rect = Rect2(64, 0, 32, 32)
+	if -112.5 < angle_deg and angle_deg <= -67.5:
+		$Head.region_rect = Rect2(96, 0, 32, 32)
+	if -67.5 < angle_deg and angle_deg <= -22.5:
+		$Head.region_rect = Rect2(128, 0, 32, 32)
+	if -22.5 < angle_deg and angle_deg <= 22.5:
+		$Head.region_rect = Rect2(160, 0, 32, 32)
+	if 22.5 < angle_deg and angle_deg <= 67.5:
+		$Head.region_rect = Rect2(192, 0, 32, 32)
+	if 67.5 < angle_deg and angle_deg <= 112.5:
+		$Head.region_rect = Rect2(224, 0, 32, 32)
+	
