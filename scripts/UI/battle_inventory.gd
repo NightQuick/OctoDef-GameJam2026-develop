280ba808ccbe6f55b3712603ee82596c0c9aae66
Prost0Iva <ivaprostoiva@gmail.com>
2026-03-19 22:08:56 +0300

    placement towers

--- scripts/UI/battle_inventory.gd
+++ scripts/UI/battle_inventory.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
 var selected_tower: Node2D
+var selected_tower_last_pos: Vector2
 var placeable: bool = true
+
+#Башни (В дальнейшем надо сделать глобальный список всех башен, а не хранить их в коде инвентаря)
+var test_tower = preload("res://nodes_scenes/towers/first_tower.tscn")
 
 func _ready() -> void:
 	pass
@@ -10,11 +14,13 @@ func _process(_delta: float) -> void:
 	if selected_tower:
 		selected_tower.global_position = $"../TileMapLayer".map_to_local($"../TileMapLayer".local_to_map($"..".get_global_mouse_position()))
 		selected_tower.global_position.y -= 8
-		$Mouse.global_position = selected_tower.global_position
+		selected_tower_last_pos = Vector2(selected_tower.global_position.x, selected_tower.global_position.y-1)
 		if not placeable:
 			selected_tower.modulate = Color(1.0, 0.0, 0.0, 1.0)
 		else: selected_tower.modulate = Color(1.0, 1.0, 1.0, .65)
 		
+		if selected_tower_last_pos != selected_tower.global_position:
+			chek_placeable()
 
 func _on_slot_template_pressed() -> void:
 	if not selected_tower:
@@ -24,11 +30,21 @@ func _on_slot_template_pressed() -> void:
 func _input(_event):
 	if Input.is_action_just_pressed("mouse_right_button"):
 		selected_tower.queue_free()
+	if Input.is_action_just_pressed("mouse_left_button"):
+		if selected_tower and placeable:
+			place_selected_tower(selected_tower.global_position)
 
-func _on_mouse_body_entered(body: Node2D) -> void:
-	if body.is_in_group("tower"):
-		placeable = false
+func chek_placeable():
+	var scene_towers: Array = get_tree().get_nodes_in_group("tower")
+	for i in scene_towers:
+		if selected_tower.global_position == i.global_position:
+			placeable = false
+			break
+		else: 
+			placeable = true
 
-func _on_mouse_body_exited(body: Node2D) -> void:
-	if body.is_in_group("tower"):
-		placeable = true
+func place_selected_tower(pos: Vector2):
+	var tower = test_tower.instantiate()
+	tower.global_position = pos
+	tower.z_index = pos.y
+	$"..".add_child(tower)
--- scripts/towers/first_tower.gd
+++ scripts/towers/first_tower.gd
@@ -2,16 +2,24 @@ extends Node2D
 
 @export var aggr_range: float = 1
 @export var attack_damage: float = 1
-@export var attack_speed: float = 1 
+@export var attack_speed: float = 1 #МАКСИМАЛЬНАЯ СКОРОСТЬ АТАКИ 0.3... иначе анимащия сдохнет
 
 var enemy_in_range: Array = []
+var target
 
 func _ready() -> void:
 	$Body/Range.scale = $Body/Range.scale*aggr_range
 
 func _process(_delta: float) -> void:
 	if len(enemy_in_range) != 0:
-		rotate_head(enemy_in_range)
+		
+		#В этой башне, цель настроена на первого вошедшего в зону поражения врага
+		target = enemy_in_range[0]
+		
+		rotate_head()
+		if $Attack.is_stopped(): $Attack.start(attack_speed)
+	else:
+		$Attack.stop()
 
 func _on_body_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
@@ -21,26 +29,53 @@ func _on_body_body_exited(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		enemy_in_range.erase(body)
 
-func rotate_head(bodies: Array):
-	#Пока что тут настройка поворота на первого врага, что вошёл в зону поражения башни
-	var dir = bodies[0].global_position - global_position
+func rotate_head():
+	var dir = target.global_position - global_position
 	var angle_rad = dir.angle()
 	var angle_deg = rad_to_deg(angle_rad)
 	
 	if 112.5 < angle_deg and angle_deg <= 157.5:
-		$Head.region_rect = Rect2(0, 0, 32, 32)
+		$Head.region_rect.position.x = 0
 	if (-157.5 <= angle_deg and angle_deg <= -180) or (157.5 < angle_deg and angle_deg <= 180):
-		$Head.region_rect = Rect2(32, 0, 32, 32)
+		$Head.region_rect.position.x = 32
 	if -157.5 < angle_deg and angle_deg <= -112.5:
-		$Head.region_rect = Rect2(64, 0, 32, 32)
+		$Head.region_rect.position.x = 64
 	if -112.5 < angle_deg and angle_deg <= -67.5:
-		$Head.region_rect = Rect2(96, 0, 32, 32)
+		$Head.region_rect.position.x = 96
 	if -67.5 < angle_deg and angle_deg <= -22.5:
-		$Head.region_rect = Rect2(128, 0, 32, 32)
+		$Head.region_rect.position.x = 128
 	if -22.5 < angle_deg and angle_deg <= 22.5:
-		$Head.region_rect = Rect2(160, 0, 32, 32)
+		$Head.region_rect.position.x = 160
 	if 22.5 < angle_deg and angle_deg <= 67.5:
-		$Head.region_rect = Rect2(192, 0, 32, 32)
+		$Head.region_rect.position.x = 192
 	if 67.5 < angle_deg and angle_deg <= 112.5:
-		$Head.region_rect = Rect2(224, 0, 32, 32)
-	
+		$Head.region_rect.position.x = 224
+
+func _on_attack_timeout() -> void:
+	var shoot_anim = create_tween()
+	var head_pos = $Head.position
+	match $Head.region_rect.position.x:
+		0.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x+2, head_pos.y-1), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		32.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x+3, head_pos.y), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		64.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x+2, head_pos.y+1), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		96.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x, head_pos.y+3), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		128.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x-2, head_pos.y+1), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		160.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x-3, head_pos.y), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		192.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x-2, head_pos.y-1), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
+		224.0:
+			shoot_anim.tween_property($Head, "position", Vector2(head_pos.x, head_pos.y-3), .1)
+			shoot_anim.tween_property($Head, "position", head_pos, .2)
