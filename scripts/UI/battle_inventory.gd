ae84958d5f646512821c53bf3b055b43dff966dd
Prost0Iva <ivaprostoiva@gmail.com>
2026-03-20 23:18:36 +0300

    tiles check

--- scripts/UI/battle_inventory.gd
+++ scripts/UI/battle_inventory.gd
@@ -6,13 +6,14 @@ var placeable: bool = true
 
 #Башни (В дальнейшем надо сделать глобальный список всех башен, а не хранить их в коде инвентаря)
 var test_tower = preload("res://nodes_scenes/towers/first_tower.tscn")
+@onready var tile_map = $"../TileMapLayer"
 
 func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
 	if selected_tower:
-		selected_tower.global_position = $"../TileMapLayer".map_to_local($"../TileMapLayer".local_to_map($"..".get_global_mouse_position()))
+		selected_tower.global_position = tile_map.map_to_local(tile_map.local_to_map($"..".get_global_mouse_position()))
 		selected_tower.global_position.y -= 8
 		selected_tower_last_pos = Vector2(selected_tower.global_position.x, selected_tower.global_position.y-1)
 		if not placeable:
@@ -36,10 +37,19 @@ func _input(_event):
 
 func chek_placeable():
 	var scene_towers: Array = get_tree().get_nodes_in_group("tower")
-	for i in scene_towers:
-		if selected_tower.global_position == i.global_position:
-			placeable = false
-			break
+	var tile_y = tile_map.get_cell_atlas_coords(tile_map.local_to_map($"..".get_global_mouse_position())).y
+	#Проверка что тайл, являеться тайлом, на который можно разместить башню
+	if tile_y != 0:
+		placeable = false
+	else:
+		#Проверка что тайл, не занят другой башней
+		if scene_towers:
+			for i in scene_towers:
+				if selected_tower.global_position == i.global_position:
+					placeable = false
+					break
+				else: 
+					placeable = true
 		else: 
 			placeable = true
 
