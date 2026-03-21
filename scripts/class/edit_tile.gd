86c5a511b8f134450ba1fde231141fbcebc0b818
Hopper <Man-popular@mail.ru>
2026-03-21 18:19:42 +0300

    add class ./scripts/edit_tile.gd and 3x fix tilemap_script

---
+++ scripts/class/edit_tile.gd
@@ -0,0 +1,24 @@
+class_name edit_tile
+
+static func place_selected_tower(tree_scene, possition: Vector2, placed_node: PackedScene, GettingTileMap: TileMapLayer):
+	var correct_possition = GettingTileMap.map_to_local(possition)
+	var node = placed_node.instantiate()
+	node.global_position = correct_possition
+	tree_scene.add_child(node)
+	print(correct_possition, " - possition of tower")
+	print(node.position, ' позиция заспавненной ноды')
+
+
+static func find_tiles_by_id(id_atlas : int, need_tiles : Vector2i, TMap : TileMapLayer):
+	var used_cells = TMap.get_used_cells()
+	var found_cells = []
+	# Сканируем их
+	for cell in used_cells:
+		var source_id = TMap.get_cell_source_id(cell) #Указывает на атлас тайла на передаваемых координатах
+		var atlas_coords = TMap.get_cell_atlas_coords(cell)
+	# Если ID совпадает с искомым
+		if (source_id == id_atlas and atlas_coords.x == need_tiles.x and atlas_coords.y == need_tiles.y):
+			found_cells.append(cell)
+			print("\n Atlas ID: ", source_id, "\n Нашел тайл ID: ", atlas_coords, "\n На координатах: ", cell)
+
+	return found_cells
--- scripts/global_scenes/tile_map_layer.gd
+++ scripts/global_scenes/tile_map_layer.gd
@@ -1,39 +1,16 @@
 extends Node
 
-@export var id_atlas = 0
-@export var need_tiles = Vector2(1, 1)
-@export var need_node = preload("res://nodes_scenes/towers/first_tower.tscn")
+@export var atlas_id : int = 0
+@export var tiles_founding : Vector2 = Vector2(1, 1)
+@export var node_spawning : PackedScene = preload("res://nodes_scenes/towers/first_tower.tscn")
 @onready var TMap : TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
-	var all_found_tiles_cord = find_tiles_by_id()
-	print(all_found_tiles_cord)
-	place_in_tiles(all_found_tiles_cord)
-
-func find_tiles_by_id():
-	var found_cells = []
-	# Получаем все занятые клетки на слое
-	var used_cells = TMap.get_used_cells()
-	# Сканируем их
-	for cell in used_cells:
-		var source_id = TMap.get_cell_source_id(cell) #Указывает на атлас тайла на передаваемых координатах
-		var atlas_coords = TMap.get_cell_atlas_coords(cell)
-	# Если ID совпадает с искомым
-		if (source_id == id_atlas and atlas_coords.x == need_tiles.x and atlas_coords.y == need_tiles.y):
-			found_cells.append(cell)
-			print("Atlas: ", source_id, " Нашел тайл ID ", atlas_coords, " на координатах: ", cell)
-
-	return found_cells
+	var a = place_in_tiles(edit_tile.find_tiles_by_id(atlas_id, tiles_founding, TMap))
+	print(a, 'Искомые тайлы')
 
 func place_in_tiles(cords_massive):
 	for placing in cords_massive:
 		print("d2", placing)
-		place_selected_tower(Vector2(placing[0], placing[1]), need_node)
-
-func place_selected_tower(possition: Vector2, placed_node: PackedScene):
-	var correct_possition = TMap.map_to_local(possition)
-	var node = placed_node.instantiate()
-	node.global_position = correct_possition
-	get_tree().current_scene.add_child(node)
-	print(correct_possition, " - possition of tower")
-	print(node.position, ' позиция заспавненной ноды')
+		#place_selected_tower(Vector2(placing[0], placing[1]), need_node)
+		edit_tile.place_selected_tower(self, Vector2(placing[0], placing[1]), node_spawning, TMap)
