917e48f150062516320df073b01363abdbdab0bc
Hopper <Man-popular@mail.ru>
2026-03-24 13:40:49 +0300

    add class path_to_scene

---
+++ scripts/class/path_to_scenes.gd
@@ -0,0 +1,5 @@
+class_name scenes_path
+extends Node2D
+
+static var evil_seven_node : PackedScene = preload("res://nodes_scenes/mobs/angry/evil_seven.tscn")
+static var target_tower_node : PackedScene = preload("res://nodes_scenes/towers/target_tower.tscn")
